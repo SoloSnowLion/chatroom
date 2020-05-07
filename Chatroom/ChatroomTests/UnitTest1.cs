aeea142bc3996ddad01d0317449af4ebce6038db
Stefan de Graaf <stefan.de.graaf.013@hotmail.com>
2020-05-07 12:59:25 +0200

    Added unit testing and optimized ajax call

---
+++ Chatroom/ChatroomTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Chatroom;
+
+namespace ChatroomTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
